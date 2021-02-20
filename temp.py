71dcb2e0d410928c28686b1f4f539d2d4efc5847
Nikita Prikhodko <prihodko.nikita.03@mail.ru>
2021-02-20 21:22:52 +0300

    Проверка значений

--- temp.py
+++ temp.py
@@ -1,17 +1,20 @@
-#!/usr/bin/env python 3.9
+
 # -*- coding: utf-8 -*-
 import sys 
 import math
 
-b=float(sys.argv[1])
-c=float(sys.argv[2])
+try:
+    b = float(sys.argv[1])    
+    c = float(sys.argv[2])
 
-d=b*b-4*c  # находим дискрименант d
+    print('x^2 + ' + sys.argv[1] + 'x + ' + sys.argv[2])
 
-print('x^2+'+sys.argv[1]+'x+'+sys.argv[2])
-    
-if d > 0:
-    print('x1='+str((-math.sqrt(b)+d)/2))
-    print('x2='+str((-math.sqrt(b)-d)/2))
-else: 
-    print("it hasn't roots")
+
+    d = b*b - 4*c  # находим дискрименант d
+        
+    if d > 0:
+        print('x1 =' + str((-b + math.sqrt(d))/2))
+        print('x2=' + str((-b - math.sqrt(d))/2))
+    else: print("it hasn't roots")
+except:
+    print("Error. Number must be float type")

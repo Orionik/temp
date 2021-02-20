8eff30462e22524979fc1663bc063623f0a09990
Nikita Prikhodko <prihodko.nikita.03@mail.ru>
2021-02-20 15:30:05 +0300

    Rules of PEP 8

--- temp.py
+++ temp.py
@@ -1,16 +1,17 @@
-import sys, math
-
+#!/usr/bin/env python 3.9
+# -*- coding: utf-8 -*-
+import sys 
+import math
 
 b=float(sys.argv[1])
-
 c=float(sys.argv[2])
 
-d=b*b-4*c
+d=b*b-4*c  # находим дискрименант d
 
-    print('x^2+'+sys.argv[1]+'x+'+sys.argv[2])
+print('x^2+'+sys.argv[1]+'x+'+sys.argv[2])
+    
 if d > 0:
     print('x1='+str((-math.sqrt(b)+d)/2))
-
     print('x2='+str((-math.sqrt(b)-d)/2))
 else: 
     print("it hasn't roots")

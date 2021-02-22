6226bacf27814f7e9ccd57fd7e326da30ecd38ba
Nikita Prikhodko <prihodko.nikita.03@mail.ru>
2021-02-22 22:30:15 +0300

    Исправил по PEP8, исправил дискриминант и подправил исключение

--- temp.py
+++ temp.py
@@ -6,15 +6,16 @@ import math
 try:
     b = float(sys.argv[1])    
     c = float(sys.argv[2])
-
-    print('x^2 + ' + sys.argv[1] + 'x + ' + sys.argv[2])
-
-
-    d = b*b - 4*c  # находим дискрименант d
-        
-    if d > 0:
-        print('x1 =' + str((-b + math.sqrt(d))/2))
-        print('x2=' + str((-b - math.sqrt(d))/2))
-    else: print("it hasn't roots")
 except:
     print("Error. Number must be float type")
+    
+print('x^2 + ' + sys.argv[1] + 'x + ' + sys.argv[2])
+
+d = b*b - 4*c  # находим дискриминант d
+        
+if d >= 0:
+    print('x1 =' + str((-b + math.sqrt(d))/2))
+    print('x2=' + str((-b - math.sqrt(d))/2))
+else: 
+    print("it hasn't roots")
+

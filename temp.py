4126eab59e8fc1600136e61f7574affea78ef224
Nikita Prikhodko <prihodko.nikita.03@mail.ru>
2021-02-22 22:41:15 +0300

    Теперь нахожу корни отдельно

--- temp.py
+++ temp.py
@@ -13,9 +13,12 @@ print('x^2 + ' + sys.argv[1] + 'x + ' + sys.argv[2])
 
 d = b*b - 4*c  # находим дискриминант d
         
+x1 = str((-b + math.sqrt(d))/2)
+x2 = str((-b - math.sqrt(d))/2)
+
 if d >= 0:
-    print('x1 =' + str((-b + math.sqrt(d))/2))
-    print('x2=' + str((-b - math.sqrt(d))/2))
+    print('x1 =' + x1)
+    print('x2 =' + x2)
 else: 
     print("it hasn't roots")
 
